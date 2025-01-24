d95dc9afc09b1ade3f1f63a65ab3c9c8b6a5bd00
spoolertech <spoolertech@gmail.com>
2025-01-24 00:26:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import { CartProvider } from './context/CartContext';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
